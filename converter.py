b611823f3eed093899eeb3072f3982de4feff530
maxminder <3309137697@qq.com>
2021-12-01 19:41:36 +0800

    bug

--- converter.py
+++ converter.py
@@ -1,16 +1,9 @@
-from jittor.utils.pytorch_converter import convert
+#from jittor.utils.pytorch_converter import convert
 import numpy as np
-#import torch
-import jittor as jt
-#from torch import jit
+import torch
+#import jittor as jt
 #from torch._C import device
-class YourDataset(jt.dataset.Dataset):
-    def __init__(self):
-        super().__init__(batch_size=32)
-        self.set_attrs(total_len=100)
 
-    def __getitem__(self, k):
-        return k, k*k
-
-x = jt.randn((2,3))
-x = x.contiguous()
+x = torch.randn((2,3,4,5))
+print(x.new_empty(0))
+#print(x)
