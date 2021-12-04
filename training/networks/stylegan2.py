8353ea31221a0979194beb19a9aad9c8ee6b87d3
maxminder <3309137697@qq.com>
2021-12-04 19:19:14 +0800

    bug

--- training/networks/stylegan2.py
+++ training/networks/stylegan2.py
@@ -352,8 +352,8 @@ class ToRGB(jt.nn.Module):
     def __init__(self, in_channel, style_dim, upsample=True, blur_kernel=[1, 3, 3, 1]):
         super().__init__()
 
-        # if upsample:
-        #     self.upsample = jt.nn.Upsample(blur_kernel)
+        if upsample:
+            self.upsample = jt.nn.Upsample(blur_kernel)
 
         self.conv = ModulatedConv2d(in_channel, 3, 1, style_dim, demodulate=False)
         self.bias = jt.nn.Parameter(jt.zeros((1, 3, 1, 1)))
@@ -362,10 +362,10 @@ class ToRGB(jt.nn.Module):
         out = self.conv(input, style)
         out = out + self.bias
 
-        # if skip is not None:
-        #     skip = self.upsample(skip)
+        if skip is not None:
+            skip = self.upsample(skip)
 
-        #     out = out + skip
+            out = out + skip
 
         return out
 
