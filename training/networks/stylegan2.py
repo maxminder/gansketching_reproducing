923a30b0b3abd73740d80c310c99f2a0ed6882ff
maxminder <3309137697@qq.com>
2021-12-04 16:53:24 +0800

    bug

--- training/networks/stylegan2.py
+++ training/networks/stylegan2.py
@@ -232,26 +232,26 @@ class ModulatedConv2d(nn.Module):
             batch * self.out_channel, in_channel, self.kernel_size, self.kernel_size
         )
 
-        # if self.upsample:
-        #     input = input.view(1, batch * in_channel, height, width)
-        #     weight = weight.view(
-        #         batch, self.out_channel, in_channel, self.kernel_size, self.kernel_size
-        #     )
-        #     weight = weight.transpose(1, 2).reshape(
-        #         batch * in_channel, self.out_channel, self.kernel_size, self.kernel_size
-        #     )
-        #     out = F.conv_transpose2d(input, weight, padding=0, stride=2, groups=batch)
-        #     _, _, height, width = out.shape
-        #     out = out.view(batch, self.out_channel, height, width)
-        #     out = self.blur(out)
+        if self.upsample:
+            input = input.view(1, batch * in_channel, height, width)
+            weight = weight.view(
+                batch, self.out_channel, in_channel, self.kernel_size, self.kernel_size
+            )
+            weight = weight.transpose(1, 2).reshape(
+                batch * in_channel, self.out_channel, self.kernel_size, self.kernel_size
+            )
+            out = F.conv_transpose2d(input, weight, padding=0, stride=2, groups=batch)
+            _, _, height, width = out.shape
+            out = out.view(batch, self.out_channel, height, width)
+            out = self.blur(out)
 
-        # elif self.downsample:
-        #     input = self.blur(input)
-        #     _, _, height, width = input.shape
-        #     input = input.view(1, batch * in_channel, height, width)
-        #     out = F.conv2d(input, weight, padding=0, stride=2, groups=batch)
-        #     _, _, height, width = out.shape
-        #     out = out.view(batch, self.out_channel, height, width)
+        elif self.downsample:
+            input = self.blur(input)
+            _, _, height, width = input.shape
+            input = input.view(1, batch * in_channel, height, width)
+            out = F.conv2d(input, weight, padding=0, stride=2, groups=batch)
+            _, _, height, width = out.shape
+            out = out.view(batch, self.out_channel, height, width)
 
         input = input.view(1, batch * in_channel, height, width)
         out = F.conv2d(input, weight, padding=self.padding, groups=batch)
