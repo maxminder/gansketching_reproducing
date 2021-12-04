c071d475494782eecff6a8027cc96b9703a0c718
maxminder <3309137697@qq.com>
2021-12-04 20:38:11 +0800

    why

--- training/dataset.py
+++ training/dataset.py
@@ -1,78 +1,78 @@
-import random
-import pathlib
-from PIL import Image
-#from torch.utils import data
-#from torch.utils.data import Dataset
-#from torchvision import transforms
-import jittor as jt
-from jittor import dataset
-from jittor import transform
+# import random
+# import pathlib
+# from PIL import Image
+# #from torch.utils import data
+# #from torch.utils.data import Dataset
+# #from torchvision import transforms
+# import jittor as jt
+# from jittor import dataset
+# from jittor import transform
 
 
-IMAGE_EXTENSIONS = {'bmp', 'jpg', 'jpeg', 'pgm', 'png', 'ppm',
-                    'tif', 'tiff', 'webp'}
-class ImagePathDataset(dataset.Dataset):
-    def __init__(self,drop_last, batch_size, path, image_mode='L', transform=None, max_images=None):
-        super.__init__(batch_size,True,drop_last)
-        path = pathlib.Path(path)
-        files = sorted([file for ext in IMAGE_EXTENSIONS
-                       for file in path.glob('*.{}'.format(ext))])
-        if max_images is None:
-            self.files = files
-        elif max_images < len(files):
-            self.files = random.sample(files, max_images)
-        else:
-            print(f"max_images larger or equal to total number of files, use {len(files)} images instead.")
-            self.files = files
-        self.transform = transform
-        self.image_mode = image_mode
+# IMAGE_EXTENSIONS = {'bmp', 'jpg', 'jpeg', 'pgm', 'png', 'ppm',
+#                     'tif', 'tiff', 'webp'}
+# class ImagePathDataset(dataset.Dataset):
+#     def __init__(self,drop_last, batch_size, path, image_mode='L', transform=None, max_images=None):
+#         super.__init__(batch_size,True,drop_last)
+#         path = pathlib.Path(path)
+#         files = sorted([file for ext in IMAGE_EXTENSIONS
+#                        for file in path.glob('*.{}'.format(ext))])
+#         if max_images is None:
+#             self.files = files
+#         elif max_images < len(files):
+#             self.files = random.sample(files, max_images)
+#         else:
+#             print(f"max_images larger or equal to total number of files, use {len(files)} images instead.")
+#             self.files = files
+#         self.transform = transform
+#         self.image_mode = image_mode
 
-    def __getitem__(self, index):
-        image_path = self.files[index]
-        image = Image.open(image_path).convert(self.image_mode)
-        if self.transform is not None:
-            image = self.transform(image)
-        return index, image
+#     def __getitem__(self, index):
+#         image_path = self.files[index]
+#         image = Image.open(image_path).convert(self.image_mode)
+#         if self.transform is not None:
+#             image = self.transform(image)
+#         return index, image
 
-    def __len__(self):
-        return len(self.files)
-
-
-def data_sampler(dataset, shuffle):
-    if shuffle:
-        return jt.dataset.RandomSampler(dataset)
-    else:
-        return jt.dataset.SequentialSampler(dataset)
+#     def __len__(self):
+#         return len(self.files)
 
 
-def create_dataloader(data_dir, size, batch, img_channel=3):
-    mean, std = [0.5 for _ in range(img_channel)], [0.5 for _ in range(img_channel)]
-    transform = jt.transform.Compose([
-            jt.transform.Resize(size),
-            jt.transform.ToTensor(),
-            jt.transform.Normalize(mean, std, inplace=True),
-        ])
-
-    if img_channel == 1:
-        image_mode = 'L'
-    elif img_channel == 3:
-        image_mode = 'RGB'
-    else:
-        raise ValueError("image channel should be 1 or 3, but got ", img_channel)
-
-    dataset = ImagePathDataset(True, batch, data_dir, image_mode, transform)
-
-    sampler = data_sampler(dataset, shuffle=True)
-    #loader = jt.dataset.DataLoader(dataset, batch_size=batch, sampler=sampler, drop_last=True)
-    loader = dataset
-    return loader, sampler
+# def data_sampler(dataset, shuffle):
+#     if shuffle:
+#         return jt.dataset.RandomSampler(dataset)
+#     else:
+#         return jt.dataset.SequentialSampler(dataset)
 
 
-def yield_data(loader, sampler, distributed=False):
-    epoch = 0
-    while True:
-        # if distributed:
-        #     sampler.set_epoch(epoch)
-        for i, batch in loader:
-            yield batch
-        epoch += 1
+# def create_dataloader(data_dir, size, batch, img_channel=3):
+#     mean, std = [0.5 for _ in range(img_channel)], [0.5 for _ in range(img_channel)]
+#     transform = jt.transform.Compose([
+#             jt.transform.Resize(size),
+#             jt.transform.ToTensor(),
+#             jt.transform.Normalize(mean, std, inplace=True),
+#         ])
+
+#     if img_channel == 1:
+#         image_mode = 'L'
+#     elif img_channel == 3:
+#         image_mode = 'RGB'
+#     else:
+#         raise ValueError("image channel should be 1 or 3, but got ", img_channel)
+
+#     dataset = ImagePathDataset(True, batch, data_dir, image_mode, transform)
+
+#     sampler = data_sampler(dataset, shuffle=True)
+#     #loader = jt.dataset.DataLoader(dataset, batch_size=batch, sampler=sampler, drop_last=True)
+#     loader = dataset
+#     return loader, sampler
+
+
+# def yield_data(loader, sampler, distributed=False):
+#     epoch = 0
+#     while True:
+#         # if distributed:
+#         #     sampler.set_epoch(epoch)
+#         for i, batch in loader:
+#             yield batch
+#         epoch += 1
--- training/networks/transform.py
+++ training/networks/transform.py
@@ -1,84 +1,84 @@
-# import torch
-# import torch.nn as nn
-# import torch.nn.parallel
-import jittor as jt
-from . import pix2pix
-from .misc import set_requires_grad
-from .diffaug import DiffAugment
+# # import torch
+# # import torch.nn as nn
+# # import torch.nn.parallel
+# import jittor as jt
+# from . import pix2pix
+# from .misc import set_requires_grad
+# from .diffaug import DiffAugment
 
 
-class RepeatChannel(jt.nn.Module):
-    def __init__(self, repeat):
-        super(RepeatChannel, self).__init__()
-        self.repeat = repeat
+# class RepeatChannel(jt.nn.Module):
+#     def __init__(self, repeat):
+#         super(RepeatChannel, self).__init__()
+#         self.repeat = repeat
 
-    def forward(self, img):
-        return img.repeat(1, self.repeat, 1, 1)
-
-
-class Downsample(jt.nn.Module):
-    def __init__(self, n_iter):
-        super(Downsample, self).__init__()
-        self.n_iter = n_iter
-
-    def forward(self, img):
-        for _ in range(self.n_iter):
-            img = jt.nn.interpolate(img, scale_factor=0.5, mode='bicubic')
-        return img
+#     def forward(self, img):
+#         return img.repeat(1, self.repeat, 1, 1)
 
 
-class Upsample(jt.nn.Module):
-    def __init__(self, n_iter):
-        super(Upsample, self).__init__()
-        self.n_iter = n_iter
+# class Downsample(jt.nn.Module):
+#     def __init__(self, n_iter):
+#         super(Downsample, self).__init__()
+#         self.n_iter = n_iter
 
-    def forward(self, img):
-        for _ in range(self.n_iter):
-            img = jt.nn.interpolate(img, scale_factor=2.0, mode='bicubic')
-        return img
+#     def forward(self, img):
+#         for _ in range(self.n_iter):
+#             img = jt.nn.interpolate(img, scale_factor=0.5, mode='bicubic')
+#         return img
 
 
-class OutputTransform(jt.nn.Module):
-    def __init__(self, opt, process='', diffaug_policy=''):
-        super(OutputTransform, self).__init__()
-        self.opt = opt
-        if diffaug_policy == '':
-            self.augment = None
-        else:
-            self.augment = DiffAugment(policy=diffaug_policy)
+# class Upsample(jt.nn.Module):
+#     def __init__(self, n_iter):
+#         super(Upsample, self).__init__()
+#         self.n_iter = n_iter
 
-        transforms = []
-        process = process.split(',')
-        for p in process:
-            if p.startswith('up'):
-                n_iter = int(p.replace('up', ''))
-                transforms.append(Upsample(n_iter))
-            elif p.startswith('down'):
-                n_iter = int(p.replace('down', ''))
-                transforms.append(Downsample(n_iter))
-            elif p == 'to3ch':
-                transforms.append(RepeatChannel(3))
-            elif p == 'toSketch':
-                sketch = self.setup_sketch(opt)
-                transforms.append(sketch)
-            else:
-                ValueError("Transforms contains unrecognizable key: %s" % p)
-        self.transforms = jt.nn.Sequential(*transforms)
+#     def forward(self, img):
+#         for _ in range(self.n_iter):
+#             img = jt.nn.interpolate(img, scale_factor=2.0, mode='bicubic')
+#         return img
 
-    def setup_sketch(self, opt):
-        sketch = pix2pix.ResnetGenerator(3, 1, n_blocks=9, use_dropout=False)
 
-        state_dict = jt.load(opt.photosketch_path, map_location='cpu')
-        if hasattr(state_dict, '_metadata'):
-            del state_dict._metadata
+# class OutputTransform(jt.nn.Module):
+#     def __init__(self, opt, process='', diffaug_policy=''):
+#         super(OutputTransform, self).__init__()
+#         self.opt = opt
+#         if diffaug_policy == '':
+#             self.augment = None
+#         else:
+#             self.augment = DiffAugment(policy=diffaug_policy)
 
-        sketch.load_state_dict(state_dict)
-        sketch.train()
-        set_requires_grad(sketch.parameters(), False)
-        return sketch
+#         transforms = []
+#         process = process.split(',')
+#         for p in process:
+#             if p.startswith('up'):
+#                 n_iter = int(p.replace('up', ''))
+#                 transforms.append(Upsample(n_iter))
+#             elif p.startswith('down'):
+#                 n_iter = int(p.replace('down', ''))
+#                 transforms.append(Downsample(n_iter))
+#             elif p == 'to3ch':
+#                 transforms.append(RepeatChannel(3))
+#             elif p == 'toSketch':
+#                 sketch = self.setup_sketch(opt)
+#                 transforms.append(sketch)
+#             else:
+#                 ValueError("Transforms contains unrecognizable key: %s" % p)
+#         self.transforms = jt.nn.Sequential(*transforms)
 
-    def forward(self, img, apply_aug=True):
-        img = self.transforms(img)
-        if apply_aug and self.augment is not None:
-            img = self.augment(img)
-        return img
+#     def setup_sketch(self, opt):
+#         sketch = pix2pix.ResnetGenerator(3, 1, n_blocks=9, use_dropout=False)
+
+#         state_dict = jt.load(opt.photosketch_path, map_location='cpu')
+#         if hasattr(state_dict, '_metadata'):
+#             del state_dict._metadata
+
+#         sketch.load_state_dict(state_dict)
+#         sketch.train()
+#         set_requires_grad(sketch.parameters(), False)
+#         return sketch
+
+#     def forward(self, img, apply_aug=True):
+#         img = self.transforms(img)
+#         if apply_aug and self.augment is not None:
+#             img = self.augment(img)
+#         return img
