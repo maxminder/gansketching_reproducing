a245f39a188144e16d7a606db2365c6d4feb0566
maxminder <3309137697@qq.com>
2021-12-04 16:44:23 +0800

    upsample

--- training/networks/stylegan2.py
+++ training/networks/stylegan2.py
@@ -27,65 +27,65 @@ def make_kernel(k):
     return k
 
 
-class Upsample(nn.Module):
-    def __init__(self, kernel, factor=2):
-        super().__init__()
+# class Upsample(nn.Module):
+#     def __init__(self, kernel, factor=2):
+#         super().__init__()
 
-        self.factor = factor
-        kernel = make_kernel(kernel) * (factor ** 2)
-        self.register_buffer("kernel", kernel)
+#         self.factor = factor
+#         kernel = make_kernel(kernel) * (factor ** 2)
+#         self.register_buffer("kernel", kernel)
 
-        p = kernel.shape[0] - factor
+#         p = kernel.shape[0] - factor
 
-        pad0 = (p + 1) // 2 + factor - 1
-        pad1 = p // 2
+#         pad0 = (p + 1) // 2 + factor - 1
+#         pad1 = p // 2
 
-        self.pad = (pad0, pad1)
+#         self.pad = (pad0, pad1)
 
-    def forward(self, input):
-        out = upfirdn2d(input, self.kernel, up=self.factor, down=1, pad=self.pad)
+#     def forward(self, input):
+#         out = upfirdn2d(input, self.kernel, up=self.factor, down=1, pad=self.pad)
 
-        return out
-
-
-class Downsample(nn.Module):
-    def __init__(self, kernel, factor=2):
-        super().__init__()
-
-        self.factor = factor
-        kernel = make_kernel(kernel)
-        self.register_buffer("kernel", kernel)
-
-        p = kernel.shape[0] - factor
-
-        pad0 = (p + 1) // 2
-        pad1 = p // 2
-
-        self.pad = (pad0, pad1)
-
-    def forward(self, input):
-        out = upfirdn2d(input, self.kernel, up=1, down=self.factor, pad=self.pad)
-
-        return out
+#         return out
 
 
-class Blur(nn.Module):
-    def __init__(self, kernel, pad, upsample_factor=1):
-        super().__init__()
+# class Downsample(nn.Module):
+#     def __init__(self, kernel, factor=2):
+#         super().__init__()
 
-        kernel = make_kernel(kernel)
+#         self.factor = factor
+#         kernel = make_kernel(kernel)
+#         self.register_buffer("kernel", kernel)
 
-        if upsample_factor > 1:
-            kernel = kernel * (upsample_factor ** 2)
+#         p = kernel.shape[0] - factor
 
-        self.register_buffer("kernel", kernel)
+#         pad0 = (p + 1) // 2
+#         pad1 = p // 2
 
-        self.pad = pad
+#         self.pad = (pad0, pad1)
 
-    def forward(self, input):
-        out = upfirdn2d(input, self.kernel, pad=self.pad)
+#     def forward(self, input):
+#         out = upfirdn2d(input, self.kernel, up=1, down=self.factor, pad=self.pad)
 
-        return out
+#         return out
+
+
+# class Blur(nn.Module):
+#     def __init__(self, kernel, pad, upsample_factor=1):
+#         super().__init__()
+
+#         kernel = make_kernel(kernel)
+
+#         if upsample_factor > 1:
+#             kernel = kernel * (upsample_factor ** 2)
+
+#         self.register_buffer("kernel", kernel)
+
+#         self.pad = pad
+
+#     def forward(self, input):
+#         out = upfirdn2d(input, self.kernel, pad=self.pad)
+
+#         return out
 
 
 class EqualConv2d(nn.Module):
@@ -184,21 +184,21 @@ class ModulatedConv2d(nn.Module):
         self.upsample = upsample
         self.downsample = downsample
 
-        if upsample:
-            factor = 2
-            p = (len(blur_kernel) - factor) - (kernel_size - 1)
-            pad0 = (p + 1) // 2 + factor - 1
-            pad1 = p // 2 + 1
+        # if upsample:
+        #     factor = 2
+        #     p = (len(blur_kernel) - factor) - (kernel_size - 1)
+        #     pad0 = (p + 1) // 2 + factor - 1
+        #     pad1 = p // 2 + 1
 
-            self.blur = Blur(blur_kernel, pad=(pad0, pad1), upsample_factor=factor)
+        #     self.blur = Blur(blur_kernel, pad=(pad0, pad1), upsample_factor=factor)
 
-        if downsample:
-            factor = 2
-            p = (len(blur_kernel) - factor) + (kernel_size - 1)
-            pad0 = (p + 1) // 2
-            pad1 = p // 2
+        # if downsample:
+        #     factor = 2
+        #     p = (len(blur_kernel) - factor) + (kernel_size - 1)
+        #     pad0 = (p + 1) // 2
+        #     pad1 = p // 2
 
-            self.blur = Blur(blur_kernel, pad=(pad0, pad1))
+        #     self.blur = Blur(blur_kernel, pad=(pad0, pad1))
 
         fan_in = in_channel * kernel_size ** 2
         self.scale = 1 / math.sqrt(fan_in)
@@ -232,32 +232,31 @@ class ModulatedConv2d(nn.Module):
             batch * self.out_channel, in_channel, self.kernel_size, self.kernel_size
         )
 
-        if self.upsample:
-            input = input.view(1, batch * in_channel, height, width)
-            weight = weight.view(
-                batch, self.out_channel, in_channel, self.kernel_size, self.kernel_size
-            )
-            weight = weight.transpose(1, 2).reshape(
-                batch * in_channel, self.out_channel, self.kernel_size, self.kernel_size
-            )
-            out = F.conv_transpose2d(input, weight, padding=0, stride=2, groups=batch)
-            _, _, height, width = out.shape
-            out = out.view(batch, self.out_channel, height, width)
-            out = self.blur(out)
+        # if self.upsample:
+        #     input = input.view(1, batch * in_channel, height, width)
+        #     weight = weight.view(
+        #         batch, self.out_channel, in_channel, self.kernel_size, self.kernel_size
+        #     )
+        #     weight = weight.transpose(1, 2).reshape(
+        #         batch * in_channel, self.out_channel, self.kernel_size, self.kernel_size
+        #     )
+        #     out = F.conv_transpose2d(input, weight, padding=0, stride=2, groups=batch)
+        #     _, _, height, width = out.shape
+        #     out = out.view(batch, self.out_channel, height, width)
+        #     out = self.blur(out)
 
-        elif self.downsample:
-            input = self.blur(input)
-            _, _, height, width = input.shape
-            input = input.view(1, batch * in_channel, height, width)
-            out = F.conv2d(input, weight, padding=0, stride=2, groups=batch)
-            _, _, height, width = out.shape
-            out = out.view(batch, self.out_channel, height, width)
+        # elif self.downsample:
+        #     input = self.blur(input)
+        #     _, _, height, width = input.shape
+        #     input = input.view(1, batch * in_channel, height, width)
+        #     out = F.conv2d(input, weight, padding=0, stride=2, groups=batch)
+        #     _, _, height, width = out.shape
+        #     out = out.view(batch, self.out_channel, height, width)
 
-        else:
-            input = input.view(1, batch * in_channel, height, width)
-            out = F.conv2d(input, weight, padding=self.padding, groups=batch)
-            _, _, height, width = out.shape
-            out = out.view(batch, self.out_channel, height, width)
+        input = input.view(1, batch * in_channel, height, width)
+        out = F.conv2d(input, weight, padding=self.padding, groups=batch)
+        _, _, height, width = out.shape
+        out = out.view(batch, self.out_channel, height, width)
 
         return out
 
@@ -340,8 +339,8 @@ class ToRGB(nn.Module):
     def __init__(self, in_channel, style_dim, upsample=True, blur_kernel=[1, 3, 3, 1]):
         super().__init__()
 
-        if upsample:
-            self.upsample = Upsample(blur_kernel)
+        # if upsample:
+        #     self.upsample = Upsample(blur_kernel)
 
         self.conv = ModulatedConv2d(in_channel, 3, 1, style_dim, demodulate=False)
         self.bias = nn.Parameter(torch.zeros(1, 3, 1, 1))
@@ -557,20 +556,20 @@ class ConvLayer(nn.Sequential):
     ):
         layers = []
 
-        if downsample:
-            factor = 2
-            p = (len(blur_kernel) - factor) + (kernel_size - 1)
-            pad0 = (p + 1) // 2
-            pad1 = p // 2
+        # if downsample:
+        #     factor = 2
+        #     p = (len(blur_kernel) - factor) + (kernel_size - 1)
+        #     pad0 = (p + 1) // 2
+        #     pad1 = p // 2
 
-            layers.append(Blur(blur_kernel, pad=(pad0, pad1)))
+        #     layers.append(Blur(blur_kernel, pad=(pad0, pad1)))
 
-            stride = 2
-            self.padding = 0
+        #     stride = 2
+        #     self.padding = 0
 
-        else:
-            stride = 1
-            self.padding = kernel_size // 2
+        # else:
+        stride = 1
+        self.padding = kernel_size // 2
 
         layers.append(
             EqualConv2d(
