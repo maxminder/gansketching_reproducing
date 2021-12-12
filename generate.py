ee86acf02d0f6de3031f69ec5c47129b92b9c00e
龚闻文 <gww19@mails.tsinghua.edu.cn>
2021-12-12 15:39:39 +0800

    delete eval

--- generate.py
+++ generate.py
@@ -3,15 +3,15 @@ import argparse
 
 import random
 import numpy as np
-import torch
-from torchvision import utils
-from training.networks.stylegan2 import Generator
+#import torch
+#from torchvision import utils
 import jittor as jt
+from training.networks.stylegan2 import Generator
 
 
 def save_image_pytorch(img, name):
     """Helper function to save torch tensor into an image file."""
-    utils.save_image(
+    jt.misc.save_image(
         img,
         name,
         nrow=1,
@@ -25,19 +25,20 @@ def generate(args, netG, device, mean_latent):
     """Generates images from a generator."""
     # loading a w-latent direction shift if given
     if args.w_shift is not None:
-        w_shift = torch.from_numpy(np.load(args.w_shift)).to(device)
+        #w_shift = torch.from_numpy(np.load(args.w_shift)).to(device)
+        w_shift = jt.float32(np.load(args.w_shift))
         w_shift = w_shift[None, :]
         mean_latent = mean_latent + w_shift
     else:
-        w_shift = torch.tensor(0., device=device)
+        w_shift = jt.float32(0.)
 
     ind = 0
-    with torch.no_grad():
-        netG.eval()
+    with jt.no_grad():
+        # netG.eval()
 
         # Generate images from a file of input noises
         if args.fixed_z is not None:
-            sample_z = torch.load(args.fixed_z, map_location=device) + w_shift
+            sample_z = jt.load(args.fixed_z) + w_shift
             for start in range(0, sample_z.size(0), args.batch_size):
                 end = min(start + args.batch_size, sample_z.size(0))
                 z_batch = sample_z[start:end]
@@ -51,8 +52,11 @@ def generate(args, netG, device, mean_latent):
         for start in range(0, args.samples, args.batch_size):
             end = min(start + args.batch_size, args.samples)
             batch_sz = end - start
-            sample_z = torch.randn(batch_sz, 512, device=device) + w_shift
-
+            sample_z = jt.randn(batch_sz, 512) + w_shift
+            # print(jt.mean(sample_z))
+            # print(jt.std(sample_z))
+            # print(args.truncation)
+            # print(mean_latent)
             sample, _ = netG([sample_z], truncation=args.truncation, truncation_latent=mean_latent)
 
             for s in sample:
@@ -81,20 +85,20 @@ if __name__ == '__main__':
     # use a fixed seed if given
     if args.seed is not None:
         random.seed(args.seed)
-        torch.manual_seed(args.seed)
-        torch.cuda.manual_seed_all(args.seed)
+        jt.set_seed(args.seed)
+        jt.set_global_seed(args.seed)
 
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
 
-    netG = Generator(args.size, 512, 8).to(device)
-    checkpoint = torch.load(args.ckpt, map_location='cpu')
+    netG = Generator(args.size, 512, 8)#.to(device)
+    checkpoint = jt.load(args.ckpt)
 
     netG.load_state_dict(checkpoint)
 
     # get mean latent if truncation is applied
     if args.truncation < 1:
-        with torch.no_grad():
+        with jt.no_grad():
             mean_latent = netG.mean_latent(args.truncation_mean)
     else:
         mean_latent = None
