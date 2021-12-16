b9d222a1a25010ad55f9800644cf340cc633fb0b
maxminder <3309137697@qq.com>
2021-12-16 15:13:53 +0800

    debug

--- training/networks/stylegan2.py
+++ training/networks/stylegan2.py
@@ -244,6 +244,7 @@ class ModulatedConv2d(jt.nn.Module):
                 batch * in_channel, self.out_channel, self.kernel_size, self.kernel_size
             )
             # out = F.conv_transpose2d(input, weight, padding=0, stride=2, groups=batch)
+            print(input.shape)
             input = jt.misc.split(input,in_channel,dim=1)
             weight = jt.misc.split(weight,in_channel,dim=0)
             result =  []
@@ -252,7 +253,6 @@ class ModulatedConv2d(jt.nn.Module):
             #out = jt.nn.conv_transpose2d(input, weight, padding=0, stride=2, groups=batch)
             out = jt.concat(result,dim=1)
             _, _, height, width = out.shape
-            print(input.shape)
             print(height,width)
 
             out = out.view(batch, self.out_channel, height, width)
