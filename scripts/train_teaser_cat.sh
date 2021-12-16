8358ea474466575072f5e8915c29bc2254133573
maxminder <3309137697@qq.com>
2021-12-16 21:05:11 +0800

    gww2

--- scripts/train_teaser_cat.sh
+++ scripts/train_teaser_cat.sh
@@ -2,8 +2,8 @@
 rm -r cache_files
 python train.py \
 --name teaser_cat_augment --batch 4 \
---dataroot_sketch ./data/sketch/by_author/cat \
---dataroot_image ./data/image/cat --l_image 0.7 \
---g_pretrained ./pretrained/stylegan2-cat/netG.pth \
---d_pretrained ./pretrained/stylegan2-cat/netD.pth \
+--dataroot_sketch ~/jittor/gansketching_reproducing/data/sketch/by_author/cat \
+--dataroot_image ~/jittor/gansketching_reproducing/data/image/cat --l_image 0.7 \
+--g_pretrained ~/jittor/gansketching_reproducing/pretrained/stylegan2-cat/netG.pth \
+--d_pretrained ~/jittor/gansketching_reproducing/pretrained/stylegan2-cat/netD.pth \
 --max_iter 150000 --disable_eval --diffaug_policy translation \
