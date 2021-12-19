e8b1780e4b0b1321ac34553b7bbc0b26a92fd704
maxminder <3309137697@qq.com>
2021-12-19 13:39:21 +0800

    transpose

--- training/networks/stylegan2.py
+++ training/networks/stylegan2.py
@@ -250,7 +250,7 @@ class ModulatedConv2d(jt.nn.Module):
             weight = weight.transpose(1, 2).reshape(
                 batch * in_channel, self.out_channel, self.kernel_size, self.kernel_size
             )
-            out = F.conv_transpose2d(input, weight, padding=0, stride=2, groups=batch)
+            # out = F.conv_transpose2d(input, weight, padding=0, stride=2, groups=batch)
             print(input.shape)
             input = jt.misc.split(input,in_channel,dim=1)
             weight = jt.misc.split(weight,in_channel,dim=0)
