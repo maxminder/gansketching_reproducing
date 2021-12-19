e17b2f2612177fb32ee4a6a340565c16e010bc98
龚闻文 <gww19@mails.tsinghua.edu.cn>
2021-12-19 19:00:50 +0800

    test

--- training/networks/op/fused_act.py
+++ training/networks/op/fused_act.py
@@ -103,6 +103,6 @@ class FusedLeakyReLU(jt.Module):
 def fused_leaky_relu(input, bias=None, negative_slope=0.2, scale=2 ** 0.5):
     if bias is not None:
         rest_dim = [1] * (input.ndim - bias.ndim - 1)
-        return jt.nn.leaky_relu(input + bias.view(1, bias.shape[0], *rest_dim), scale=0.2) * scale
+        return jt.nn.leaky_relu(input + bias.view((1, bias.shape[0], *rest_dim)), scale=0.2) * scale
     else:
         return jt.nn.leaky_relu(input, scale=0.2) * scale
