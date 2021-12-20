a51dbaa4f87240852ad12b15d2daf227b5590198
maxminder <3309137697@qq.com>
2021-12-21 00:56:37 +0800

    church

--- scripts/train_church.sh
+++ scripts/train_church.sh
@@ -1,7 +1,8 @@
+rm -r ./cache_files
 python train.py \
 --name church_augment --batch 1 \
 --dataroot_sketch ~/jittor/gansketching_reproducing/data/sketch/photosketch/gabled_church \
 --dataroot_image ~/jittor/gansketching_reproducing/data/image/church --l_image 0.7 \
 --g_pretrained ~/jittor/gansketching_reproducing/pretrained/stylegan2-church/netG.pth \
 --d_pretrained ~/jittor/gansketching_reproducing/pretrained/stylegan2-church/netD.pth \
---max_iter 150000 --disable_eval --diffaug_policy translation --no_wandb \
+--max_iter 150001 --disable_eval --diffaug_policy translation --no_wandb \
