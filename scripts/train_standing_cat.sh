37ee6958d5d38b86de3d0756012e6ce55a4f6a40
maxminder <3309137697@qq.com>
2021-12-21 14:15:39 +0800

    train

--- scripts/train_standing_cat.sh
+++ scripts/train_standing_cat.sh
@@ -1,6 +1,6 @@
 rm -r cache_files
 python train.py \
---name teaser_cat_augment --batch 1 \
+--name standing_cat_augment --batch 1 \
 --dataroot_sketch ~/jittor/gansketching_reproducing/data/sketch/photosketch/standing_cat \
 --dataroot_image ~/jittor/gansketching_reproducing/data/image/cat --l_image 0.7 \
 --g_pretrained ~/jittor/gansketching_reproducing/pretrained/stylegan2-cat/netG.pth \
