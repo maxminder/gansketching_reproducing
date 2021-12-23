d7fdcb29820f22659577d8f56b81986a4b5a7400
maxminder <3309137697@qq.com>
2021-12-24 00:11:20 +0800

    resume_standing_cat

--- scripts/train_standing_cat.sh
+++ scripts/train_standing_cat.sh
@@ -1,8 +1,8 @@
 rm -r cache_files
 python train.py \
---name standing_cat_noaugment --batch 1 \
+--name standing_cat_augment --batch 1 \
 --dataroot_sketch ~/jittor/gansketching_reproducing/data/sketch/photosketch/standing_cat \
 --dataroot_image ~/jittor/gansketching_reproducing/data/image/cat --l_image 0.7 \
 --g_pretrained ~/jittor/gansketching_reproducing/pretrained/stylegan2-cat/netG.pth \
 --d_pretrained ~/jittor/gansketching_reproducing/pretrained/stylegan2-cat/netD.pth \
---max_iter 150001 --disable_eval --no_wandb --resume_iter 32500\
+--max_iter 150001 --disable_eval --no_wandb --diffaug_policy translation --resume_iter 77500\
