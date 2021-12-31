2a511fe1f9e487663495b907e8254cfed8bfe241
龚闻文 <gww19@mails.tsinghua.edu.cn>
2021-12-31 19:45:19 +0800

    test

--- training/networks/op/time.py
+++ training/networks/op/time.py
@@ -3,15 +3,16 @@ import jittor as jt
 from jittor.models import resnet50
 jt.flags.use_cuda = jt.has_cuda
 
-warmup = 10
-rerun = 100
-batch_size = 8
-data = jt.random((batch_size, 3, 224, 224))
+warmup = 100
+rerun = 1000
+batch_size = 50
+data = jt.random((batch_size, 3, 256, 256))
+kernel = jt.random((4, 4))
 
 # 此段代码对jittor进行热身，确保时间测试准确
 jt.sync_all(True)
 for i in range(warmup):
-    pred = upfirdn2d(data)
+    pred = upfirdn2d(data, kernel)
     # sync是把计算图发送到计算设备上
     pred.sync()
 # sync_all(true)是把计算图发射到计算设备上，并且同步。
@@ -21,7 +22,7 @@ jt.sync_all(True)
 # 开始测试运行时间
 start = time.time()
 for i in range(rerun):
-    pred = upfirdn2d(data)
+    pred = upfirdn2d(data, kernel)
     pred.sync()
 jt.sync_all(True)
 end = time.time()
