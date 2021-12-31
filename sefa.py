12c586a55806993fa95cf0172b7e9c98e1a794fc
maxminder <3309137697@qq.com>
2022-01-01 00:58:05 +0800

    sefa

--- sefa.py
+++ sefa.py
@@ -1,10 +1,21 @@
+# import os
+# prefix = 'python editor.py --obj cat --scalar 15 --ckpt /root/jittor/gansketching_reproducing/weights/photosketch_standing_cat_noaug.pth --slice 3 --layers '
+# layers = ['1,4','5,9','10,18']
+# next = ' --comp_id '
+# comp_id = range(50)
+# next2 = ' --save_dir output/sefa_standing_cat_'
+# for i in comp_id:
+#     for layer in layers:
+#         command = prefix + layer + next + str(i) + next2 + str(i) + '/' + layer
+#         os.system(command)
 import os
 prefix = 'python editor.py --obj cat --scalar 15 --ckpt /root/jittor/gansketching_reproducing/weights/photosketch_standing_cat_noaug.pth --slice 3 --layers '
-layers = ['1,4','5,9','10,18']
+layers = ['5,9']
 next = ' --comp_id '
-comp_id = range(50)
-next2 = ' --save_dir output/sefa_standing_cat_'
-for i in comp_id:
+comp_id = 2
+samples = range(50)
+next2 = ' --save_dir output/test'
+for i in samples:
     for layer in layers:
-        command = prefix + layer + next + str(i) + next2 + str(i) + '/' + layer
+        command = prefix + layer + next + str(comp_id) + next2 + str(i)
         os.system(command)
